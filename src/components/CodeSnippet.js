54f7809ceb032823fafa513aeb3cd2fba0275dd5
wevs <68214494+dopewevmond@users.noreply.github.com>
2023-02-01 17:23:53 +0000

    copy code snippet into console

--- src/components/CodeSnippet.js
+++ src/components/CodeSnippet.js
@@ -4,38 +4,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaste } from '@fortawesome/pro-light-svg-icons'
 
 
-function copyToClipboard(text) {
-  var dummy = document.createElement("textarea");
-  document.body.appendChild(dummy);
-  dummy.setAttribute("id", "dummy_id");
-  document.getElementById("dummy_id").value = text;
-  dummy.select();
-  document.execCommand("copy");
-  document.body.removeChild(dummy);
+function copyToConsole(text) {
+  const textarea = document.querySelector(".cli");
+  if (textarea) {
+    textarea.value = text;
+    textarea.focus();
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
+  }
 }
 
 class CodeSnippet extends React.Component {
-
   copy() {
-    copyToClipboard(this.props.code);
+    copyToConsole(this.props.code);
   }
 
- render() {
-    return(
-      <div className='code-snippet'>
+  render() {
+    return (
+      <div className="code-snippet">
         <pre>
-          <code>
-            {this.props.code}
-          </code>
-            <div className='icon-container'>
-              <button onClick={this.copy.bind(this)}>
-                <FontAwesomeIcon icon={faPaste}/>
-              </button>
-            </div>
+          <code>{this.props.code}</code>
+          <div className="icon-container">
+            <button onClick={this.copy.bind(this)}>
+              <FontAwesomeIcon icon={faPaste} />
+            </button>
+          </div>
         </pre>
       </div>
-    )
+    );
   }
 }
 
-export default CodeSnippet
+export default CodeSnippet;
